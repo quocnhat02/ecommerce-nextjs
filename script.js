d7f0e4302a35b391248ac7621b44915800e8fefe
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-11-03 20:14:42 +0700

    beginner react 1

---
+++ script.js
@@ -0,0 +1,25 @@
+// var, let and const
+
+// var name; // Declaring a variable
+// name = 'zeno'; // assigning a value
+
+// var name = 'zeno';
+// var name = 'john';
+
+// console.log(name);
+
+// let
+// Cannot be redeclare
+// let name;
+// name = 'zero';
+// name = 'one';
+
+// console.log(name);
+
+// const
+// Cannot be redeclare
+// Cannot be reassigned
+
+const name = 'hero';
+
+console.log(name);
