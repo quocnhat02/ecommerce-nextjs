3c2b54427d0dd20a63a30f3755811860aa1a11ad
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-11-03 21:45:17 +0700

    creating a custom component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <HelloWorld />
+    </div>
+  );
+};
+
+const HelloWorld = () => {
+  return <h1>Hello World</h1>;
 };
 
 export default App;
