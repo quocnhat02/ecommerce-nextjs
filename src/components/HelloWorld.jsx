19a5631a1bbdfbc0148f1c1934b3f2100b6a3100
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-11-03 22:16:31 +0700

    outputting dynamic values

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const HelloWorld = () => {
-  return <div>Hello World</div>;
+  const name = 'Nhat';
+  const age = 20;
+  //   const person = {
+  //     name: 'Saitama',
+  //   };
+
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>I am {age} years old</h2>
+      <h2>{Math.floor(Math.random() * 100)}</h2>
+    </div>
+  );
 };
 
 export default HelloWorld;
