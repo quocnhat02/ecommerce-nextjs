4e1c84521cbfb3ef37fe2cf4262d7f2b0eb1033c
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-11-04 21:34:40 +0700

    props destructuring

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
-const User = (props) => {
+const User = ({ name, job }) => {
   return (
     <div>
       <div className="user">
-        <h2>Name: {props.name || 'Default name'}</h2>
-        <h3>Job: {props.job || 'Default job'}</h3>
+        <h2>Name: {name}</h2>
+        <h3>Job: {job}</h3>
       </div>
     </div>
   );
 };
 
-// User.defaultProps = {
-//   name: 'Default name',
-//   job: 'Default job',
-// };
+User.defaultProps = {
+  name: 'Default name',
+  job: 'Default job',
+};
 
 export default User;
