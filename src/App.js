71a40b3a65aed0e482b3d00098cb83297753711d
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-11-05 08:34:30 +0700

    profile app-install react icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ProfileList from './components/profile/ProfileList';
+import { AiOutlineGithub } from 'react-icons/ai';
 
 const App = () => {
   return (
     <div>
       <ProfileList />
+      <AiOutlineGithub />
     </div>
   );
 };
