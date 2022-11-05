9129b501f7d9d4415c5841b44ad270ecf3b1ef75
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-11-05 11:13:31 +0700

    profile app-create profiles props

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from './Profile.module.css';
-import profile1 from '../../assets/profile1.png';
 import {
   AiOutlineTwitter,
   AiOutlineGithub,
@@ -9,24 +8,24 @@ import {
 import { IconContext } from 'react-icons';
 import Card from '../UI/card/Card';
 
-const Profile = () => {
+const Profile = ({ image, name, job, company, link }) => {
   return (
     <Card>
       <div className={styles.profile}>
-        <img src={profile1} alt="" />
+        <img src={image} alt="" />
         <div className={styles['profile-content']}>
           <h3>My Profile</h3>
           <div className={styles.text}>
             <p>Name:</p>
-            <p>Adaora Nwodo</p>
+            <p>{name}</p>
           </div>
           <div className={styles.text}>
             <p>Job:</p>
-            <p>Cloud Engineer</p>
+            <p>{job}</p>
           </div>
           <div className={styles.text}>
             <p>Company:</p>
-            <p>Microsoft</p>
+            <p>{company}</p>
           </div>
 
           <IconContext.Provider value={{ color: '#666', size: '20px' }}>
@@ -38,7 +37,7 @@ const Profile = () => {
           </IconContext.Provider>
 
           <div className={styles.btn}>
-            <a href="#" target="_blank" rel="noreferrer">
+            <a href={link} target="_blank" rel="noreferrer">
               View Profile
             </a>
           </div>
--- src/components/profile/ProfileList.jsx
+++ src/components/profile/ProfileList.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Profile from './Profile';
 import styles from './ProfileList.module.css';
+import profile1 from '../../assets/profile1.png';
+import profile2 from '../../assets/profile2.png';
+import profile3 from '../../assets/profile3.png';
 
 const ProfileList = () => {
   return (
@@ -8,7 +11,31 @@ const ProfileList = () => {
       <div>
         <h1>Team Members</h1>
         <div className={styles['profile-container']}>
-          <Profile />
+          <Profile
+            image={profile1}
+            name={'Adora Nwodo'}
+            job={'Cloud Engineer'}
+            company={'Microsoft'}
+            link={'https://twitter.com/AdoraNwodo'}
+          />
+        </div>
+        <div className={styles['profile-container']}>
+          <Profile
+            image={profile2}
+            name={'John Doe'}
+            job={'Web Developer'}
+            company={'Google'}
+            link={'#'}
+          />
+        </div>
+        <div className={styles['profile-container']}>
+          <Profile
+            image={profile3}
+            name={'Fisayo Tinuke'}
+            job={'Mobile Developer'}
+            company={'ZinoTrust'}
+            link={'#'}
+          />
         </div>
       </div>
     </section>
