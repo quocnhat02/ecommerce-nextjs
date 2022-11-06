1c5cfa56f5cb946621cd4a820b5e3e19f216f876
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-11-06 18:50:34 +0700

    useState 12

--- src/components/UseStateObject.jsx
+++ src/components/UseStateObject.jsx
@@ -7,6 +7,10 @@ const UseStateObject = () => {
     company: 'Google',
   });
 
+  const updateCompany = () => {
+    setProfile({ ...profile, company: 'Microsoft' });
+  };
+
   return (
     <>
       <h2 className="--text-center --my2">useState in Object</h2>
@@ -14,6 +18,11 @@ const UseStateObject = () => {
         <h2>Name: {profile.name}</h2>
         <h4>Job: {profile.job}</h4>
         <h4>Company: {profile.company}</h4>
+      </div>
+      <div className="--center-all --my2">
+        <button className="--btn --btn-primary" onClick={updateCompany}>
+          Change Company
+        </button>
       </div>
     </>
   );
