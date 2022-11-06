4ec73c98b132da65f974f0f4e7e5dec14f2edc54
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-11-06 18:44:54 +0700

    useState 11

--- src/components/UseStateObject.jsx
+++ src/components/UseStateObject.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import { useState } from 'react';
 
 const UseStateObject = () => {
-  return <div>UseStateObject</div>;
+  const [profile, setProfile] = useState({
+    name: 'John Doe',
+    job: 'Web Dev',
+    company: 'Google',
+  });
+
+  return (
+    <>
+      <h2 className="--text-center --my2">useState in Object</h2>
+      <div className="--card --mx2">
+        <h2>Name: {profile.name}</h2>
+        <h4>Job: {profile.job}</h4>
+        <h4>Company: {profile.company}</h4>
+      </div>
+    </>
+  );
 };
 
 export default UseStateObject;
