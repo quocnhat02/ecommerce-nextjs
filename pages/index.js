ed7ca24fa58a14b1603515d8f0b9ca76a03667ab
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-01-04 18:10:12 +0700

    scss

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import { Inter } from '@next/font/google';
-import styles from '../styles/Home.module.css';
-
-const inter = Inter({ subsets: ['latin'] });
+import styles from '../styles/Home.module.scss';
 
 export default function Home() {
   return (
@@ -14,7 +11,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Welcome</h1>
+      <h1 className={styles.red}>Welcome</h1>
     </>
   );
 }
