42b6eb31c1dca0ebaa833f01bb13459692d0ecdd
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-01-05 20:32:09 +0700

    ad top

---
+++ components/header/Top.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable @next/next/no-img-element */
+import styles from './styles.module.scss';
+import { MdSecurity } from 'react-icons/md';
+import { BsSuitHeart } from 'react-icons/bs';
+import { RiAccountPinCircleLine } from 'react-icons/ri';
+import { RiArrowDropDownFill } from 'react-icons/ri';
+import Link from 'next/link';
+
+const Top = () => {
+  return (
+    <div className={styles.top}>
+      <div className={styles.top__container}>
+        <div></div>
+        <ul className={styles.top__list}>
+          <li>
+            <img
+              src="https://static.vecteezy.com/system/resources/thumbnails/011/571/331/small/circle-flag-of-vietnam-free-png.png"
+              alt="vietnam"
+            />
+            <span>Vietnam / usd</span>
+          </li>
+          <li>
+            <MdSecurity />
+            <span>Buyer Protection</span>
+          </li>
+          <li>
+            <span>Customer Service</span>
+          </li>
+          <li>
+            <span>Help</span>
+          </li>
+          <li>
+            <BsSuitHeart />
+            <Link href="/profile/wishlist">
+              <span>Wishlist</span>
+            </Link>
+          </li>
+          <li>
+            <div className={styles.flex}>
+              <RiAccountPinCircleLine />
+              <span>Account</span>
+              <RiArrowDropDownFill />
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Top;
