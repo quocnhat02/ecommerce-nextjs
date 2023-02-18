3b47194223fe1883eda59328819aca860a01e33a
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-02-18 21:18:37 +0700

    ad header

---
+++ components/header/index.js
@@ -0,0 +1,12 @@
+import Ad from './Ad';
+import styles from './styles.module.scss';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <Ad />
+    </header>
+  );
+};
+
+export default Header;
